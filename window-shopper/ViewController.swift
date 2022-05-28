6324b923006f3a30e27fc9551454c94ab4754548
TearsOprah <qmeshokp@mail.ru>
2022-05-28 11:31:26 +0300

    Initial Commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by meshok on 28.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
